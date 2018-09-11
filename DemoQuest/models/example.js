b0528b08047463580af9096fa84a5d569d117155
Extrasy <luisalexpm@gmail.com>
2018-09-11 11:28:09 -0700

    slight update

--- DemoQuest/models/example.js
+++ DemoQuest/models/example.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Deaths = sequelize.define("Deaths", {
     name: DataTypes.STRING,
     causeOfDeath: DataTypes.TEXT,
-    dateCreated
+    // dateCreated: DataTypes.DATE
   });
   return Deaths;
 };
