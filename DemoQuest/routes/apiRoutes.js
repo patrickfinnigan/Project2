ab81e5c6bb14bbdfa8d28de3b5d04481aacd902c
Extrasy <luisalexpm@gmail.com>
2018-09-13 19:59:23 -0700

    working

--- DemoQuest/routes/apiRoutes.js
+++ DemoQuest/routes/apiRoutes.js
@@ -2,23 +2,41 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/all", function(req, res) {
+    var query = {};
+    db.Death.findAll({
+        where: query
+    }).then(function(dbDeath) {
+      res.json(dbDeath);
+    }).catch(function(err){
+      console.log(err);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/new", function(req, res) {
+    db.Death.create(req.body).then(function(results) {
+      console.log("results", results);
+      res.json(results);
     });
   });
-
+  // Create a new example
+  app.post("/api/newVictim", function(req, res) {
+    db.Death.create(req.body).then(function(results) {
+      console.log("results", results);
+      res.json(results);
+    });
+  });
+  app.get("/api/deathCount", function(req, res) {
+     
+    db.Death.findAll({}).then(function(dbDeath) {
+      res.json(dbDeath);
+    });
+  });
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(game_top) {
+      res.json(game_top);
     });
   });
 };
